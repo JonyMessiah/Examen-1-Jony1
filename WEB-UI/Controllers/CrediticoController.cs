76dabdb7ae84c121b6dd97d40161a9350971b0d0
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2023-11-23 18:39:48 -0600

    a

---
+++ WEB-UI/Controllers/CrediticoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_UI.Controllers
+{
+    public class CrediticoController : Controller
+    {
+        public IActionResult Creditico()
+        {
+            return View();
+        }
+    }
+}
