639007cd4d749b8db40d873bd74e0fb206b680e5
Jonytheus <109642412+JonyMessiah@users.noreply.github.com>
2023-11-24 01:21:46 -0600

    u

---
+++ WEB-UI/Scripts/Creditico.js
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UI.Scripts
+{
+    public class Creditico : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
